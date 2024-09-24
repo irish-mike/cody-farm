cdc8cd4750eeab25877b59528567dffeb0214066
Michael Grinnell <michaelgr@live.ie>
2024-09-24 15:10:08 +0100

    Update env and config params

--- app/Game/Data/Constants.php
+++ app/Game/Data/Constants.php
@@ -6,20 +6,9 @@ class Constants
 {
     // Game States
     const GAME_STATE_NOT_INITIALIZED = 0;
-    const GAME_STATE_PLAYERS_REGISTERING = 1;
-    const GAME_STATE_IN_PROGRESS = 2;
-    const GAME_STATE_ENDED = 3;
+    const GAME_STATE_IN_PROGRESS = 1;
+    const GAME_STATE_ENDED = 2;
 
-    // Verdicts
-    const VERDICT_DRAW = 0;
-    const VERDICT_BASED_ON_POINTS = 1;
-    const VERDICT_BASED_ON_RYO_COUNT = 2;
-    const VERDICT_TURN_TIMEOUT = 3;
-    const VERDICT_MATCHMAKING_TIMEOUT = 4;
-    const VERDICT_PLAYER_SURRENDERED = 5;
-    const VERDICT_PLAYER_DEMOLISHED = 6;
-    const VERDICT_GAME_TIMEOUT = 7;
-    const VERDICT_GAME_CANCELLED = 8;
 
     // Game Modes
     const GAME_MODE_SANDBOX = 0;
--- config/services.php
+++ config/services.php
@@ -1,38 +1,11 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Third Party Services
-    |--------------------------------------------------------------------------
-    |
-    | This file is for storing the credentials for third party services such
-    | as Mailgun, Postmark, AWS and more. This file provides the de facto
-    | location for this type of information, allowing packages to have
-    | a conventional file to locate the various service credentials.
-    |
-    */
-
-    'postmark' => [
-        'token' => env('POSTMARK_TOKEN'),
+    'game' => [
+        'ckey' => env('GAME_CKEY'),
+        'api_url' => env('GAME_API_URL', 'https://game.codyfight.com'),
+        'matchmaking_timeout' => env('GAME_MATCHMAKING_TIMEOUT', 300),
+        'polling_interval' => env('GAME_POLLING_INTERVAL', 2),
     ],
-
-    'ses' => [
-        'key' => env('AWS_ACCESS_KEY_ID'),
-        'secret' => env('AWS_SECRET_ACCESS_KEY'),
-        'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
-    ],
-
-    'resend' => [
-        'key' => env('RESEND_KEY'),
-    ],
-
-    'slack' => [
-        'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
-        ],
-    ],
-
 ];
+
